5d03ee1986aedbea3f8c38cc6c9811bc516c2a54
Victor Horta <victorhorta@gmail.com>
2014-10-25 13:33:22 -0200

    Added offline log capture method

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,9 +1,20 @@
 package main;
 
+import org.jnetpcap.Pcap;
+
 public class Main {
 
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
+        String fname = "myfile.pcap";
+        StringBuilder errbuf = new StringBuilder();
+        
+
+        Pcap pcap = Pcap.openOffline(fname, errbuf);
+        if (pcap == null) {
+          System.err.printf("Error while opening device for capture: "
+            + errbuf.toString());
+          return;
+        }
 
     }
 
